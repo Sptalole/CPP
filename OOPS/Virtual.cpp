25ea90b464db37c4eb6b5540b2fc25607cec85d4
Sptalole <sagartalole15@gmail.com>
2025-09-27 00:37:39 +0530

    added output

--- OOPS/Virtual.cpp
+++ OOPS/Virtual.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class Base {
+class Base
+{
 public:
-      void show()
-     {
+    void show()
+    {
         std::cout << "Base show" << std::endl;
     }
 };
 
-class Derived : public Base {
+class Derived : public Base
+{
 public:
-    void show()  {
+    void show()
+    {
         std::cout << "Derived show" << std::endl;
     }
 };
 
-int main(){
+int main()
+{
 
-Base* obj = new Derived;
-//Derived* obj = new Derived;
-//Base* obj = new Base;
-obj->show();  // Output: "Derived show"
+    Base *obj = new Base;
+    obj->show(); // Output: "Base show"
 
-return 0;
+    /*
+    Base *obj = new Derived;
+    obj->show(); // Output: "Base show"
+    */
+
+    /*
+    Derived* obj = new Derived;
+    obj->show(); // Output: "Derived show"
+    */
+
+    return 0;
 }
