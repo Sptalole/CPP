b833990723af3769a5b2465ef179e0d5649803eb
Sptalole <sagartalole15@gmail.com>
2025-09-26 12:20:17 +0530

    added output

--- OOPS/copyconst.cpp
+++ OOPS/copyconst.cpp
@@ -1,45 +1,44 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-
-class Hero{
-public: 
+class Hero
+{
+public:
     int a;
     char ch;
     char *name;
 
-Hero(){
-    cout << "address of =   default constructor" << this << endl;
-    name = new char[100];
-    
-}
+    Hero()
+    {
+        cout << "default constructor called " << endl;
+        cout << "address of = default constructor is : " << this << endl;
+        name = new char[100];
+    }
 
+    Hero(Hero &orgobj)
+    {
+        cout << "copy constructor called" << endl;
+        this->a = orgobj.a;
 
+        char *cha = new char[strlen(orgobj.name + 1)];
+        strcpy(cha, orgobj.name);
+        this->name = cha;
+    }
 
-Hero(Hero &orgobj){
-    cout <<"copy" <<endl;
-    this->a = orgobj.a;
+    void setname(char name[])
+    {
+        strcpy(this->name, name);
+    }
 
-    char *cha = new char[strlen(orgobj.name +1)];
-    strcpy(cha, orgobj.name);
-    this->name = cha;
-}
-
-void setname(char name[]){
-    strcpy(this->name, name);
-}
-
-void print(){
-cout << "name->" << name << endl;
-}
-
-
-
+    void print()
+    {
+        cout << "name:- " << name << endl;
+    }
 };
 
-
-int main(){
+int main()
+{
     Hero h1;
     h1.a = 40;
     char name[7] = "sagar";
@@ -47,13 +46,26 @@ int main(){
     h1.print();
 
     Hero h2(h1);
-    cout << "h2.a->" << h2.a << endl;
+    cout << "h2.a:- " << h2.a << endl;
     h1.name[0] = 'm';
     h1.print();
     cout << endl;
     h2.print();
-    
-    cout << "address of hero1 " << &h1 << endl;
+
+    cout << "address of hero1:- " << &h1 << endl;
 
     return 0;
 }
+
+/*OUTPUT*/
+/*
+default constructor called
+address of = default constructor is : 0xd0d55ffba0
+name:- sagar
+copy constructor called
+h2.a:- 40
+name:- magar
+
+name:- sagar
+address of hero1:- 0xd0d55ffba0
+*/
