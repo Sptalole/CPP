eaa4bcb841a154b22870413e946de67b997b3d3c
Sptalole <sagartalole15@gmail.com>
2025-09-27 00:37:20 +0530

    added output

--- OOPS/DerivedClass.cpp
+++ OOPS/DerivedClass.cpp
@@ -1,38 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Person{
+class Person
+{
 public:
     string name;
     int age;
-    //Person(){cout << "default parent constructor called"<<endl;}
-    Person(string name, int age){
+    // Person(){cout << "default parent constructor called"<<endl;}
+    Person(string name, int age)
+    {
         this->name = name;
         this->age = age;
     }
 };
 
-class Student : public Person{
-    public:
-
+class Student : public Person
+{
+public:
     int rollno;
-    Student(string name, int age,int rollno) : Person( name, age){
-        this -> rollno = rollno;
+    Student(string name, int age, int rollno) : Person(name, age)
+    {
+        this->rollno = rollno;
     }
-    void getinfo(){
-        cout <<"name " << name<<  endl;
-        cout <<"age " << age <<endl;
-        cout <<"rollno " << rollno << endl;
-        
+    void getinfo()
+    {
+        cout << "name " << name << endl;
+        cout << "age " << age << endl;
+        cout << "rollno " << rollno << endl;
     }
-}; 
+};
 
-int main(){
+int main()
+{
     Student s1("rahul kumar", 20, 44);
     s1.getinfo();
-
-
-
-
     return 0;
 }
+
+/*OUTPUT*/
+/*
+name rahul kumar
+age 20
+rollno 44
+*/
