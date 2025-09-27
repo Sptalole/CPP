e6971ee86da03ba9509be4c5b59839fd29d59c66
Sptalole <sagartalole15@gmail.com>
2025-09-28 00:05:10 +0530

    Added output

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void display(vector<int> v)
 {
-    for (int i:v)
+    for (int i : v)
     {
         cout << i << " ";
     }
@@ -15,19 +15,19 @@ int main()
 {
 
     vector<int> v;
-    cout << "cap = " << v.capacity() << " size "<< v.size() << endl;
+    cout << "cap = " << v.capacity() << " size " << v.size() << endl;
     v.push_back(5);
-    cout << "cap = " << v.capacity() << " size "<< v.size() << endl;
+    cout << "cap = " << v.capacity() << " size " << v.size() << endl;
     v.push_back(6);
-    cout << "cap = " << v.capacity() << " size "<< v.size() << endl;
+    cout << "cap = " << v.capacity() << " size " << v.size() << endl;
     v.push_back(7);
-    cout << "cap = " << v.capacity() << " size "<< v.size() << endl;
-   // v.at(1);
-    //cout << "cap = " << v.capacity() << " size "<< v.size() << endl;
-    cout << v.at(0) <<endl;
-    cout << v.front()<< endl;
-    cout << v.back()<< endl;
-    cout << v.data()<< endl;
+    cout << "cap = " << v.capacity() << " size " << v.size() << endl;
+    // v.at(1);
+    // cout << "cap = " << v.capacity() << " size "<< v.size() << endl;
+    cout << v.at(0) << endl;
+    cout << v.front() << endl;
+    cout << v.back() << endl;
+    cout << v.data() << endl;
     display(v);
 
     //     vector<int>
@@ -53,3 +53,16 @@ int main()
     // cout << "Size ->" << vec1.size() << endl;
     // cout << "Capacity ->" << vec1.capacity() << endl;
 }
+
+/*OUTPUT*/
+/*
+cap = 0 size 0
+cap = 1 size 1
+cap = 2 size 2
+cap = 4 size 3
+5
+5
+7
+0x2619979d9b0
+5 6 7
+*/
